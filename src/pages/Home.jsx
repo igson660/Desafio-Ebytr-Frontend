50bbd19e9b122c731109550c2fddb3986a311425
igson <igson660@gmail.com>
2021-11-04 15:39:52 -0500

    configura style-components para estilizar o projeto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect , useState } from 'react'
 import Header from '../components/Header';
-import { getAllTasks } from '../service/requests'
+import { getAllTasks, addTask, excludeTask } from '../service/requests'
 
 function Home() {
   const [isLoading, setIsLoading] = useState(false);
   const [tasks, setTasks] = useState([])
+  const [task, setTask] = useState()
 
+  const handTasks = async () => {
+    const data = await getAllTasks();
+    setTasks(data);
+    setIsLoading(false);
+  }
+  
   useEffect(() => {
     setIsLoading(true)
-    async function handTasks() {
-      const data = await getAllTasks();
-      setTasks(data);
-      setIsLoading(false);
-    }
     handTasks();
   }, []);
 
@@ -22,18 +24,32 @@ function Home() {
       { isLoading ? <p>carregango...</p> : null }
       <section>
         <h1>Adcionar tarefa</h1>
-        <input type="text" placeholder="Insira uma Tarefa" />
-        <button type="button">Criar</button>
+        <input
+          type="text"
+          placeholder="Insira uma Tarefa"
+          value={ task }
+          onChange={ (e) => setTask(e.target.value)}
+          />
+        <button
+          type="submit"
+          onClick={ () => addTask(task) }
+          >
+            Criar
+          </button>
       </section>
       <section>
         <h1>Todas a Tarefas</h1>
           <ul>
           {
-            tasks.map((task) => (
+            tasks.map(({ Task, _id }) => (
               <>
-                <li>{ task.Task }</li>
+                <li>{ Task }</li>
                 <button>Editar</button>
-                <button>Remover</button>
+                <button
+                type="submit"
+                onClick={ () => excludeTask(_id) }
+                >Remover</button>
+                
               </>
             ))
           }
--- src/service/requests.js
+++ src/service/requests.js
@@ -1,10 +1,51 @@
 export async function getAllTasks() {
   try {
-   const tasks = fetch('http://localhost:3001/')
+    const tasks = fetch('http://localhost:3001/')
     .then(r => r.json())
     .then(({tasks}) => tasks);
     
     return tasks;
+  } catch (error) {
+    return null;
+  }
+};
+
+export function addTask(task) {
+  const body = { task: task }
+  console.log(JSON.stringify(body));
+
+  try {
+    fetch('http://localhost:3001/', {
+      method: "POST",
+      header: { "Content-Type": "application/json" },
+      body: JSON.stringify(body)
+    });
+  } catch (error) {
+    return null;
+  }
+};
+
+export async function UpdateTask() {
+  try {
+    const tasks = fetch('http://localhost:3001/')
+    .then(r => r.json())
+    .then(({tasks}) => tasks);
+    
+    return tasks;
+  } catch (error) {
+    return null;
+  }
+};
+
+export function excludeTask(id) {
+  const data = { _id: id}
+
+  try {
+    fetch('http://localhost:3001/', {
+      method: "DELETE",
+      header: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
   } catch (error) {
     return null;
   }
