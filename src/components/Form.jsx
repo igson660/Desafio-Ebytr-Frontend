43f64806a569048d6edce5e4f4928d2903af67d7
igson <igson660@gmail.com>
2021-11-04 19:30:13 -0500

    estiliza o componente form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,20 +4,19 @@ function Form() {
   return (
     <Conteiner>
       <form>
-        <fieldset>
-
-        <legend>Status</legend>
-
-        <input name="status" type="radio" id="statusPen"/>
-        <label for= "statusPen"> Pendente: </label>
-
-        <input name="status" type="radio" id="statusAnd"/>
-        <label for= "statusAnd"> Em Andamento: </label>
-
-        <input name="status" type="radio" id="statusPron"/>
-        <label for= "statusPron"> Pronto: </label>
+        <div>
+          <input name="status" type="radio" id="statusPen"/>
+          <label for= "statusPen"> Pendente </label>
+        </div>
+        <div>
+          <input name="status" type="radio" id="statusAnd"/>
+          <label for= "statusAnd"> Em Andamento </label>
+        </div>
+        <div>
+          <input name="status" type="radio" id="statusPron"/>
+          <label for= "statusPron"> Pronto </label>
+        </div>
         
-        </fieldset>
       </form>
     </Conteiner>
   );
--- src/styles/form.js
+++ src/styles/form.js
@@ -1,7 +1,19 @@
 import styled from 'styled-components';
 
 export const Conteiner = styled.div`
-  width: 90%;
+  width: 100%;
   align-content: center;
   font-size: 10px;
+
+  form {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  div{
+    margin: 3px;
+  }
 `;
